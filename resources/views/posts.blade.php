b93d5e3661a4d7c9d079c378926e996560d9aca7
ravish <ravishsomai808@gmail.com>
2021-12-01 19:05:27 +0100

    Added posts page

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+</head>
+<body>
+<article>
+    <h1><a href="/post">My first blog post</a></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur error incidunt iusto quam sapiente. Minus.</p>
+</article>
+
+<article>
+    <h1><a href="/post">My Second blog post</a></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus amet commodi consequatur culpa deserunt fugiat harum perferendis possimus quo tempore?</p>
+</article>
+
+</body>
+</html>
