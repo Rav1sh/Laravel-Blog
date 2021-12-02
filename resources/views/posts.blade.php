2adb46eeb52ae2c2250da24449bcee075fda4dbf
ravish <ravishsomai808@gmail.com>
2021-12-02 18:48:51 +0100

    Changed posts with blade components

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,15 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog</title>
-</head>
-<body>
-<article>
-    <h1><a href="/post">My first blog post</a></h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur error incidunt iusto quam sapiente. Minus.</p>
-</article>
+<x-layout>
+    @foreach($posts as $post)
+        <article>
+            <h1>
+                <a href="/posts/{{ $post->id }}">
+                    {{ $post->title }}
+                </a>
+            </h1>
 
-<article>
-    <h1><a href="/post">My Second blog post</a></h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus amet commodi consequatur culpa deserunt fugiat harum perferendis possimus quo tempore?</p>
-</article>
-
-</body>
-</html>
+            <div>
+                {{ $post->excerpt }}
+            </div>
+        </article>
+    @endforeach
+</x-layout>
