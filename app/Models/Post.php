af47bb2cef01712c00a293bea7684a2f913e719f
ravish <ravishsomai808@gmail.com>
2021-12-15 21:24:36 +0100

    Added category to each corresponding post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,6 +10,7 @@ class Post extends Model
     use HasFactory;
 
     protected $guarded = [];
+//    protected $fillable = ['title', 'excerpt', 'body', 'id'];
 
     public function category()
     {
--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,14 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('posts/{post}', function ($id) {
+//Route::get('posts/{post}', function ($id) {
+//    return view('post', [
+//        'post' => Post::findOrFail($id)
+//    ]);
+//});
+
+Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', [
-        'post' => Post::findOrFail($id)
+        'post' => $post
     ]);
 });
