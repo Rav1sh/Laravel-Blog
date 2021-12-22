7a180c5d554ddba6e1ea3fe878571fb3cce72e52
ravish <ravishsomai808@gmail.com>
2021-12-22 21:40:47 +0100

    Added authors page which shows the belongs that belongs to the specific author

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::with('category')->get()
+        'posts' => Post::latest('published_at')->with('category', 'author')->get()
     ]);
 });
 
@@ -38,3 +39,10 @@ Route::get('categories/{category:slug}', function (Category $category) {
         'posts' => $category->posts
     ]);
 });
+
+Route::get('authors/{author:name}', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts
+    ]);
+});
+
