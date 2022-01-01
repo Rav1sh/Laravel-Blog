57c3ab8e24e89835c65f311ab7fef2277eb02d4f
ravish <ravishsomai808@gmail.com>
2022-01-01 13:02:46 +0100

    Solved n+1 problem on category and author page

--- routes/web.php
+++ routes/web.php
@@ -36,13 +36,13 @@ Route::get('posts/{post:slug}', function (Post $post) {
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts->load(['category', 'author'])
     ]);
 });
 
 Route::get('authors/{author:name}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts
+        'posts' => $author->posts->load(['category', 'author'])
     ]);
 });
 
