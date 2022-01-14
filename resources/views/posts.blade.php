1a94304e5fc6b9be0189a8a7414da8273282461b
ravish <ravishsomai808@gmail.com>
2022-01-14 14:00:54 +0100

    Added category filter

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,22 +2,18 @@
         @include('_post-header')
 
         <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-            <x-post-featured-card :post="$posts[0]" />
+            @if ($posts->count())
+                <x-post-featured-card :post="$posts[0]" />
 
-            <div class="lg:grid lg:grid-cols-2">
-                @foreach ($posts->skip(1) as $post)
-                <x-post-card :post="$posts[1]"/>
-                @endforeach
-            </div>
-
-{{--            <div class="lg:grid lg:grid-cols-3">--}}
-{{--                <x-post-card/>--}}
-{{--                <x-post-card/>--}}
-{{--                <x-post-card/>--}}
-{{--        </div>--}}
+                @if($posts->count() > 1)
+                    <div class="lg:grid lg:grid-cols-6">
+                        @foreach ($posts->skip(1) as $post)
+                            <x-post-card :post="$post" class=" {{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2'}}"/>
+                        @endforeach
+                    </div>
+                @endif
+            @else
+                <p class="text-center">No Posts displayed</p>
+            @endif
     </main>
-
-
-
-
 </x-layout>
