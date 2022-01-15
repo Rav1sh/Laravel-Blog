221cd4c5d97c10f8cb387eab91905139b42d368c
ravish <ravishsomai808@gmail.com>
2022-01-15 13:22:05 +0100

    Added search filter for the body and the text field will show the value

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,8 @@ Route::get('/', function () {
     $posts = Post::latest('published_at');
 
     if (request('search')) {
-        $posts->where('title', 'like', '%' . request('search') . '%');
+        $posts->where('title', 'like', '%' . request('search') . '%')
+        ->orWhere('body', 'like', '%' . request('search') . '%');
     }
 
     return view('posts', [
